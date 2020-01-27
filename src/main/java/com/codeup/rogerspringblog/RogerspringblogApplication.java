a8d738b073079e5f26585b0afaf6627650d29de7
Roger Mayer <rmayer1984@gmail.com>
2020-01-27 09:57:13 -0600

    set up repo

---
+++ src/main/java/com/codeup/rogerspringblog/RogerspringblogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.rogerspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RogerspringblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RogerspringblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/rogerspringblog/RogerspringblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.rogerspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RogerspringblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
