a4b00451bf8779edfb0ded48f1bd0941eec45b56
Roger Mayer <rmayer1984@gmail.com>
2020-01-28 09:53:18 -0600

    completed posts ex

--- src/main/java/com/codeup/rogerspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/rogerspringblog/controllers/PostController.java
@@ -1,22 +1,33 @@
 package com.codeup.rogerspringblog.controllers;
 
+import com.codeup.rogerspringblog.models.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
+@Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String postIndex(){
-        return "posts index page";
+    public String postIndex(Model model){
+        ArrayList<Post> postList = new ArrayList<Post>();
+        postList.add(new Post(2, "Title 2", "Description 2"));
+        postList.add(new Post(3, "Title 3", "Description 3"));
+        model.addAttribute("posts", postList);
+        return "posts/index";
 
     }
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String viewPost(@PathVariable String id){
-        return "posts individual post of " + id;
+    public String viewPost(@PathVariable long id, Model model){
+        Post post1 = new Post(id, "title 1", "description 1");
+        model.addAttribute("title", post1.getTitle());
+        model.addAttribute("body", post1.getBody());
+        return "posts/show";
 
     }
     @GetMapping("/posts/create")
--- src/main/java/com/codeup/rogerspringblog/models/Post.java
+++ src/main/java/com/codeup/rogerspringblog/models/Post.java
@@ -1,8 +1,15 @@
-package com.codeup.rogerspringblog.controllers;
+package com.codeup.rogerspringblog.models;
 
 public class Post {
    private String title;
    private String body;
+   private long id;
+
+   public Post(long id, String title, String body){
+      this.id = id;
+      this.title = title;
+      this.body = body;
+   }
 
    public String getTitle() {
       return title;
@@ -18,5 +25,11 @@ public class Post {
       this.body = body;
    }
 
+   public long getId() {
+      return id;
+   }
 
+   public void setId(long id) {
+      this.id = id;
+   }
 }
