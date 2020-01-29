e18c95cfb6290f14c194867bf96d0f77d97073de
Roger Mayer <rmayer1984@gmail.com>
2020-01-29 15:28:27 -0600

    fixing routing

--- src/main/java/com/codeup/rogerspringblog/controllers/DiceController.java
+++ src/main/java/com/codeup/rogerspringblog/controllers/DiceController.java
@@ -3,11 +3,9 @@ package com.codeup.rogerspringblog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 @Controller
--- src/main/java/com/codeup/rogerspringblog/controllers/HomeController.java
+++ src/main/java/com/codeup/rogerspringblog/controllers/HomeController.java
@@ -2,10 +2,7 @@ package com.codeup.rogerspringblog.controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
