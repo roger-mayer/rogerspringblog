f01164609a180ba0b672e6d7412b68e08a33c044
Roger Mayer <rmayer1984@gmail.com>
2020-01-31 14:01:02 -0600

    post CRUD functional. linked navbar. styling cards

--- src/main/java/com/codeup/rogerspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/rogerspringblog/controllers/PostController.java
@@ -37,72 +37,43 @@ public class PostController {
     }
 
     @GetMapping("/posts/edit")
-    public String editPostForm(
-            @RequestParam String postTitle,
-            @RequestParam String postBody,
-            @RequestParam Long postId, Model model) {
-        model.addAttribute("postTitle", postTitle);
-        model.addAttribute("postBody", postBody);
-        model.addAttribute("postId", postId);
+    public String editPostForm
+            (@RequestParam Long id,
+           Model model){
+        model.addAttribute("post", postDao.getOne(id));
         return "posts/edit";
-
     }
+
 
     @PostMapping("/posts/edit")
-    public String editPost(
-            @RequestParam String newTitle,
-            @RequestParam String newBody,
-            @RequestParam long sameId) {
-        Post updatePost = new Post(sameId, newTitle, newBody);
-        postDao.save(updatePost);
-        return "redirect:/posts";
-    }
-
-
-    @PostMapping("/posts/delete")
-    public String deletePostById(@RequestParam Long postId, Model model) {
-        postDao.deleteById(postId);
-        return "redirect:/posts";
-    }
-
-    //!CREATE
-    //model param
-//    @GetMapping("/posts/create")
-//    public String createForm(Model model) {
-//        model.addAttribute("post", new Post());
-//        return "create";
-//    }
-
-    @GetMapping("/posts/create")
-    public String createForm() {
-        return "create";
-    }
-
-
-    @PostMapping("/posts/create")
-    public String createPost(
-            @RequestParam(name = "title") String title,
-            @RequestParam String description,
-            Model model
-    ) {
-//        Post post = new Post(title, description, userDao.getB
-        Post post = new Post(title, description);
-        User user = userDao.getOne(1L);
-        post.setUser(user);
+    public String editPost(@ModelAttribute Post post){
         postDao.save(post);
         return "redirect:/posts";
     }
 
 
-//    @PostMapping("/posts/create")
-//    public String submitPost(@RequestParam String title, @RequestParam String description) {
-//        long random = (long) ((Math.random() * 3) + 1);
-//        Post newPost = new Post(title, description, userDao.findById(random));
-//        User user = userDao.findById(random);
-//        user.getPosts().add(newPost);
-//        postDao.save(newPost);
-//        return "redirect:/posts";
-//    }
+    //!CREATE
+    @GetMapping("/posts/create")
+    public String createForm(Model model) {
+        model.addAttribute("post", new Post());
+        return "create";
+    }
+
+    @PostMapping("/posts/create")
+    public String createPost(@ModelAttribute Post post){
+        User user = userDao.getOne(1L);
+        post.setUser(user);
+        postDao.save(post);
+        return "redirect:/posts";
+
+    }
+
+    //!DELETE
+    @PostMapping("/posts/delete")
+    public String deletePostById(@RequestParam Long postId, Model model) {
+        postDao.deleteById(postId);
+        return "redirect:/posts";
+    }
 
     @GetMapping("/posts/{id}/details")
     public String returnOneToOneView(@PathVariable long id, Model model){
