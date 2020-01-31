ec45b7b0d844ed5c2423ede5a7173d02a9597fcf
Roger Mayer <rmayer1984@gmail.com>
2020-01-31 17:05:05 -0600

    styling edit form

--- src/main/java/com/codeup/rogerspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/rogerspringblog/controllers/PostController.java
@@ -1,16 +1,15 @@
 package com.codeup.rogerspringblog.controllers;
 
-import com.codeup.rogerspringblog.exception.PostException;
 import com.codeup.rogerspringblog.models.Post;
 import com.codeup.rogerspringblog.models.User;
 import com.codeup.rogerspringblog.repositories.PostImageRepository;
 import com.codeup.rogerspringblog.repositories.PostRepository;
 import com.codeup.rogerspringblog.repositories.UserRepository;
+import com.codeup.rogerspringblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -19,11 +18,15 @@ public class PostController {
     private final PostRepository postDao;
     private final UserRepository userDao;
     private final PostImageRepository postImageDao;
+    private final EmailService emailServiceDao;
 
-    public PostController(PostRepository postDao, UserRepository userDao, PostImageRepository postImageDao) {
+
+    public PostController(PostRepository postDao, UserRepository userDao, PostImageRepository postImageDao, EmailService emailServiceDao) {
         this.postDao = postDao;
         this.userDao = userDao;
         this.postImageDao = postImageDao;
+        this.emailServiceDao = emailServiceDao;
+
     }
 
     //! SHOW ALL
@@ -64,8 +67,8 @@ public class PostController {
         User user = userDao.getOne(1L);
         post.setUser(user);
         postDao.save(post);
+        emailServiceDao.prepareAndSend(post,"You just made a post","you just made a post");
         return "redirect:/posts";
-
     }
 
     //!DELETE
@@ -80,5 +83,20 @@ public class PostController {
         model.addAttribute("post", postDao.findById(id));
         return "posts/show";
     }
+
+    //!EMAIL
+//    @GetMapping("/posts/email")
+//    public String createEmail(@PathVariable long id, Model model) {
+//        model.addAttribute("email", emailServiceDao.findById(id));
+//        return "profile";
+//    }
+
+
+//    @GetMapping("/posts/email")
+//    @ResponseBody
+//    public String sendEmail(@RequestParam long id, Model model){
+//        emailService.prepareAndSend(emailServiceDao.getOne(1L), "subject","description");
+//        return "Sending email...";
+//    }
 
 }
---
+++ src/main/java/com/codeup/rogerspringblog/repositories/EmailServiceRepository.java
@@ -0,0 +1,22 @@
+//package com.codeup.rogerspringblog.repositories;
+//
+//import com.codeup.rogerspringblog.models.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface EmailServiceRepository extends JpaRepository<User, Long> {
+//    User findById(long id);
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
